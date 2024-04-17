b1efbf2498ba81606a5325ba8c008e352296eeef
arizustudio <arizu.team@gmail.com>
2024-04-17 16:39:46 +0700

    fix ig downloader

--- function/func.php
+++ function/func.php
@@ -123,6 +123,32 @@ function detectCommand($text) {
     preg_match($pattern, $text, $matches);
     return $matches;
 }
+function igDownload1($url){
+	$headers = array();
+	$headers[] = 'Cache-Control:no-cache';	
+	$headers[] = 'authority: api.sssgram.com';	
+	$headers[] = 'method: GET';	
+	$headers[] = 'path: /st-tik/ins/dl?url='.$url.'&timestamp=1713346223995';	
+	$headers[] = 'scheme: https';	
+	$headers[] = 'Accept: application/json, text/plain, */*';	
+	$headers[] = 'Origin: https://www.sssgram.com';	
+	$headers[] = 'Referer: https://www.sssgram.com/';	
+	$headers[] = 'Sec-Ch-Ua: "Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"';	
+	$headers[] = 'Sec-Ch-Ua-Mobile: ?0';	
+	$headers[] = 'Sec-Ch-Ua-Platform: "Windows"';	
+	$headers[] = 'Sec-Fetch-Dest: empty';	
+	$headers[] = 'Sec-Fetch-Mode: cors';	
+	$headers[] = 'Sec-Fetch-Site: same-site';	
+	$headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36';	
+    $ch = curl_init("https://api.sssgram.com/st-tik/ins/dl?url=$url&timestamp=1713346223995");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+    curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);	
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0); 
+	curl_setopt($ch, CURLOPT_TIMEOUT, 99999);
+    echo $response = curl_exec($ch);
+}
 function getVideo($urlvideo){
 	$uri = cobaltserv();
 	$bbx = array();
@@ -147,6 +173,11 @@ function getVideo($urlvideo){
 	curl_setopt($ch, CURLOPT_TIMEOUT, 99999);
     $response = curl_exec($ch);
     curl_close($ch);
+    $parse = parse_url($urlvideo);
+    $genada = json_decode($response,true);
+    if(!$genada['url'] && $parse['host'] == 'instagram.com'){
+        $response = igDownload($urlvideo);
+    }
     return $response;
 }
 function getVideo69($urlvideo){
@@ -173,6 +204,11 @@ function getVideo69($urlvideo){
 	curl_setopt($ch, CURLOPT_TIMEOUT, 99999);
     $response = curl_exec($ch);
     curl_close($ch);
+    $parse = parse_url($urlvideo);
+    $genada = json_decode($response,true);
+    if(!$genada['url'] && $parse['host'] == 'instagram.com'){
+        $response = igDownload($urlvideo);
+    }
     return $response;
 }
 function getYTMusic($urlvideo){
--- function/test.php
+++ function/test.php
@@ -1,7 +1,31 @@
 <?php
-$baraa = shell_exec('tiktok-uploader -v ../files/video/videos2814_1706751114.mp4 -d "hai" -c ../data/tiktok_cookies/baju.txt');
-if(strpos($baraa,"py test.py")){
-    echo "a";
-}else{
-    echo "b";
+
+function igDownload($url){
+	$headers = array();
+	$headers[] = 'Cache-Control:no-cache';	
+	$headers[] = 'authority: api.sssgram.com';	
+	$headers[] = 'method: GET';	
+	$headers[] = 'path: /st-tik/ins/dl?url='.$url.'&timestamp=1713346223995';	
+	$headers[] = 'scheme: https';	
+	$headers[] = 'Accept: application/json, text/plain, */*';	
+	$headers[] = 'Origin: https://www.sssgram.com';	
+	$headers[] = 'Referer: https://www.sssgram.com/';	
+	$headers[] = 'Sec-Ch-Ua: "Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"';	
+	$headers[] = 'Sec-Ch-Ua-Mobile: ?0';	
+	$headers[] = 'Sec-Ch-Ua-Platform: "Windows"';	
+	$headers[] = 'Sec-Fetch-Dest: empty';	
+	$headers[] = 'Sec-Fetch-Mode: cors';	
+	$headers[] = 'Sec-Fetch-Site: same-site';	
+	$headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36';	
+    $ch = curl_init("https://api.sssgram.com/st-tik/ins/dl?url=$url&timestamp=1713346223995");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+    curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);	
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0); 
+	curl_setopt($ch, CURLOPT_TIMEOUT, 99999);
+    echo $response = curl_exec($ch);
 }
+$url = 'http://google.com/dhasjkdas/sadsdds/sdda/sdads.html';
+$parse = parse_url($url);
+echo $parse['host'];
