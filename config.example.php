d40dfab655a6e6060a7772da786a827b7153b4e9
Galang Yoga Pradana <55178769+mzcoder-hub@users.noreply.github.com>
2024-07-12 15:23:37 +0700

    Config for IG Uploader
    Create Config for IG Uploader

---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'username' => 'username',
+    'password' => 'password',
+    'video_folder' => 'file path',
+    'log_file' => 'uploaded_videos.log',
+    'delay' => 35 * 60, // Delay in seconds
+];
+?>
